04e2e86b6039420e7bce92a18b9471836841ca49
Adam Campbell <awjc@iastate.edu>
2015-04-21 20:52:43 +0100

    add images

--- Images_2/.convertalltogifs.sh
+++
@@ -1,5 +0,0 @@
-./.converttogifs.sh ceres
-./.converttogifs.sh earth
-./.converttogifs.sh sun
-./.converttogifs.sh traj
-./.converttogifs.sh vesta
--- Images_2/getLastImage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/Dropbox/Java_Projects/DawnImager/Images/$1
-LASTFILE=$(ls -rt1 | grep -G '[\.png|\.jpg|\.gif]$' | awk '{ f=$NF }; END{ print f }')
-
-echo $LASTFILE
